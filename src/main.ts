9a8db80b0a77197ac98c7d27b6ecc73afc642a76
finchfry94 <nicholas.finch05@gmail.com>
2026-02-17 22:55:21 -0500

    inject vercel

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import { Analytics } from "@vercel/analytics/next";
+import { inject } from "@vercel/analytics";
 import App from "./App.vue";
 import router from "./router";
 import { isTauri } from "./services/codeRunner";
